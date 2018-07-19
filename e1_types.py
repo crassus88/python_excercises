3359595809575a87588890c69fa5707671259161
crassus88 <balazs.miko@ericsson.com>
2018-07-19 17:19:57 +0200

    some more on strings

--- e1_types.py
+++ e1_types.py
@@ -7,14 +7,14 @@ def print_type(x):
     print("{0} is a(n) {1}".format(x, type(x)))
 
 
-print('The "type() function prints an object\'s type"')  # Don't pay attention to the string constant now
+print('The "type() function returns an object\'s type"')  # Don't pay attention to the string constant now
 i = 2
 print_type(i)
 
 print("-"*20)
 
 print("In python 2.X '/' means integer division if both operands are int and float division if at least one of them is float")
-print("In python 3.X it is always float division")
+print("In python 3.X it is always float division. In python 2.X this is achieved with the truediv() function.")
 a = i/3
 b = i/2
 c = i/2.0
@@ -52,3 +52,37 @@ The character 'r' before a string denotes a "raw string". This means that charac
 are treated like simple characters. Such character sequences are \ and {}."""
 print(s)
 print("\nTriple quoted strings also serve the special purpose of documenting functions, as you can see in the function above")
+
+print("-"*20)
+
+s = """A string is considered a list of characters thus it has some features in common with lists.
+Fpr example characters in a string can be accessed by their position in the string via indexing."""
+print(s)
+print(s[0])
+
+s = "Slicing is a neat feature in python. It is indexing a whole range at a time."
+print(s)
+print((s[13:17] + ", isn't it!?").capitalize())
+
+print("-"*20)
+
+s = """As seen in the previous example, strings can be added together. Another way of combining strings is the format() 
+function. You can put placeholders in your string that will be populated when calling this function on said string.
+format() actually has a whole mini language for specifying the appearance of the data put in these placeholders.
+Read more about using format() at {}
+Although parts of the documentation may read like chinese at first, it always has examples to clarify usage.
+""".format("https://docs.python.org/3/library/string.html")
+print(s)
+
+s = """Throughout this code you can see that strings can not only be added together - which is called concatenation - 
+but also multiplied by integers. They can not be divided however. Or be subtracted from one-another. And they also can
+not be added to other types of objects."""
+print(s)
+try:
+    print(s + 2)
+except:
+    traceback.print_exc()
+
+print("-"*20)
+
+# todo string functions starting with capitalize
